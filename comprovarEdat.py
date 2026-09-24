37fc3ede95d2defc4ea3e9907690f4519f028d57
ITB2627-JoverNoelia <noelia.jover.7ea@itb.cat>
2026-09-24 08:30:32 +0200

    Primer commit de la practica

---
+++ comprovarEdat.py
@@ -0,0 +1,7 @@
+# Programa para comprobar la edad de un usuario
+edad = int(input("Por favor, introduce tu edad: "))
+
+if edad >= 18:
+    print("Eres mayor de edad. Puedes acceder al sistema.")
+else:
+    print("Eres menor de edad. Acceso denegado.")
---
+++ helloWorld.py
@@ -0,0 +1,2 @@
+# Mi primer programa en el ITB
+print("Hola Mundo! Este es el proyecto de pruebas de MDS.")
